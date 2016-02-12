ab100177700d609a2037b49ae41d5c7ea18223d7
Thomas Schell <thom.schell@gmail.com>
2016-02-12 01:18:14 +0100

    changed msg for support

--- demo/demo.js
+++ demo/demo.js
@@ -2,5 +2,5 @@ import Speech from '../src/speech.js';
 
 Speech.init();
 
-let text = (Speech.browserSupport()) ? 'Hurray, our browser supports speech synthesis' : 'Your browser does NOT support speech synthesis, try Chrome of Safari !';
+let text = (Speech.browserSupport()) ? 'Hurray, your browser supports speech synthesis' : 'Your browser does NOT support speech synthesis, try Chrome of Safari !';
 document.getElementById("support").innerHTML = text;
