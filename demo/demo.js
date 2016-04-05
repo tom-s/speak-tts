2c42fe0e71917067e77b90c147d30e32e40de0e5
Thomas Schell <thom.schell@gmail.com>
2016-04-05 12:29:03 +0200

    fixes

--- demo/demo.js
+++ demo/demo.js
@@ -1,37 +1,37 @@
-var Speech = require('../src/speak-tts.js');
+import Speech from '../src/speak-tts.js'
 
-function _addVoicesList(voices) {
-	let list = window.document.createElement('div');
-	let html = '<h2>Available Voices</h2><select id="languages"><option value="">autodetect language</option>';
-	_.forEach(voices, (voice) => {
-		html += '<option value="' + voice.lang + '"">' + voice.name + ' (' + voice.lang + ')</option>';
-	});
-	list.innerHTML = html;
-	window.document.body.appendChild(list);
+const _addVoicesList = (voices) => {
+	const list = window.document.createElement('div')
+	let html = '<h2>Available Voices</h2><select id="languages"><option value="">autodetect language</option>'
+	voices.forEach((voice) => {
+		html += '<option value="' + voice.lang + '"">' + voice.name + ' (' + voice.lang + ')</option>'
+	})
+	list.innerHTML = html
+	window.document.body.appendChild(list)
 }
 
 function _prepareSpeakButton() {
-	const speakButton = document.getElementById('play');
-	const textarea = document.getElementById('text');
-	const languages = document.getElementById('languages');
+	const speakButton = document.getElementById('play')
+	const textarea = document.getElementById('text')
+	const languages = document.getElementById('languages')
 	speakButton.addEventListener('click', () => {
-		Speech.setLanguage(languages.value);
+		Speech.setLanguage(languages.value)
 		Speech.speak({
 			text: textarea.value
-		});
-	});
+		})
+	})
 }
 
 Speech.init({
 	onVoicesLoaded: (data) => {
-		console.log("loaded voices", data.voices);
-		_addVoicesList(data.voices);
-		_prepareSpeakButton();
+		console.log("loaded voices", data.voices)
+		_addVoicesList(data.voices)
+		_prepareSpeakButton()
 		Speech.speak({
 			text: 'Hello, how are you today ?'
-		});
+		})
 	}
 });
 
-let text = (Speech.browserSupport()) ? 'Hurray, your browser supports speech synthesis' : "Your browser does NOT support speech synthesis. Try using Chrome of Safari instead !";
-document.getElementById("support").innerHTML = text;
+const text = (Speech.browserSupport()) ? 'Hurray, your browser supports speech synthesis' : "Your browser does NOT support speech synthesis. Try using Chrome of Safari instead !"
+document.getElementById("support").innerHTML = text
