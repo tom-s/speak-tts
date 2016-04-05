f0b5c9881c5905dd6cf12161bb36b2cb1be39950
Thomas Schell <thom.schell@gmail.com>
2016-04-05 12:19:58 +0200

    added deploy

---
+++ webpack.production.config.js
@@ -0,0 +1,36 @@
+// webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+var node_modules = path.resolve(__dirname, 'node_modules');
+
+var config = {
+    entry: {
+        demo: [ path.resolve(__dirname, 'demo/demo.js') ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'demo/build'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        alias: {}
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                  plugins: ['transform-runtime'],
+                  presets: ['es2015']
+                }
+            },
+            {
+                include: /\.json$/, loaders: ["json-loader"],
+                extensions: ['', '.json', '.js']
+            }
+        ],
+    }
+};
+
+module.exports = config;
